914178997852612434fea9cfd706130312b469f9
Daniel Goldberg <dgoldber@login1.ls6.tacc.utexas.edu>
2023-01-26 10:33:07 -0600

    update of files with working oad petsc compilation

--- lonestar_scripts/make_ls6.sh
+++ lonestar_scripts/make_ls6.sh
@@ -1,25 +1,10 @@
 
-# Switch compilers as  the Cray compiler gives an error.
-#module swap PrgEnv-gnu PrgEnv-intel
-#module swap PrgEnv-cray PrgEnv-intel
-##module swap cray-netcdf netcdf
-#module load netcdf-nc-max-vars
-#module load cray-petsc
-
-
-#module restore PrgEnv-cray
-#module load cray-hdf5-parallel/1.12.0.2
-#module load cray-netcdf-hdf5parallel/4.7.4.2
-
 
 source ~/.bashrc
 module purge
 module load intel/19.1.1 impi/19.0.9 netcdf/4.6.2 
-module load petsc/3.15-uni
 module load tacc-singularity
 
-#ROOTDIR=/home/dgoldber/MITgcm
-export LD_LIBRARY_PATH=/work/n02/n02/dngoldbe/petsc/lib:$CRAY_LD_LIBRARY_PATH:$LD_LIBRARY_PATH
 build_dir=build
 code_dir=code
 
@@ -40,12 +25,13 @@ ln ../$code_dir/cost_test.F.snap_noBglen ../$code_dir/cost_test.F
 
 
 make CLEAN
-$ROOTDIR/tools/genmake2 -mods='../code' -of=$OLDPWD/linux_amd64_ifort+mpi_lonestar6  -oad -mpi --oadsingularity "$sing_str"
+$ROOTDIR/tools/genmake2 -mods='../code' -of=$OLDPWD/linux_amd64_ifort+mpi_lonestar6_ownpetsc  -oad -mpi --oadsingularity "$sing_str"
 
 #$ROOTDIR/tools/genmake2 -ieee -mods='../code ../newcode' -of=$ROOTDIR/tools/build_options/linux_amd64_gfortran -mpi
 #$ROOTDIR/tools/genmake2 -mods='../code' -mpi
-cp $PETSC_DIR/milan-uni/include/*.mod ./
-echo $LD_LIBRARY_PATH
+
+# will need to adapt this path to yours
+cp $OWN_PETSC_DIR/arch-linux-c-opt/include/*.mod ./
 make adAll
 
 # Switch Programming Environment back
